fb355e13abf3fef11c5098091726f5b88232ab14
Savanna Denega Machado PERSONAL <savanna.denega@hotmail.com>
2018-04-23 01:43:27 -0300

    Parte GameLevels do Jogo funcionando

--- Jogo/GameLevel.cpp
+++ Jogo/GameLevel.cpp
@@ -56,17 +56,20 @@ void GameLevel::init(std::vector<std::vector<GLuint>> tileData, GLuint levelWidt
 {
 	// Calculate dimensions
 	GLuint height = tileData.size();
-	GLuint width = tileData[0].size(); // Note we can index vector at [0] since this function is only called if height > 0
+	std::vector<GLuint> rowX;
+	rowX = tileData[0];
+	GLuint width = rowX.size(); // Note we can index vector at [0] since this function is only called if height > 0
 	GLfloat unit_width = levelWidth / static_cast<GLfloat>(width), unit_height = levelHeight / height;
-	// Initialize level tiles based on tileData		
-	for (int y  = 0; y < height; ++y)
+	// Initialize level tiles based on tileData
+
+	for (size_t y  = 0; y < height; ++y)
 	{
-		for (int x = 0; x < width; ++x)
+		for (size_t x = 0; x < width; ++x)
 		{
 			// Check block type from level data (2D level array)
-			GLuint numberyx = tileData[y][x];
-			GLuint number = 1;
-			if (numberyx == number) // Solid
+			rowX = tileData[y];
+			GLuint numberX = rowX[x];
+			if (numberX == static_cast<GLint>(1)) // Solid
 			{
 				glm::vec2 pos(unit_width * x, unit_height * y);
 				glm::vec2 size(unit_width, unit_height);
@@ -74,16 +77,16 @@ void GameLevel::init(std::vector<std::vector<GLuint>> tileData, GLuint levelWidt
 				obj.IsSolid = GL_TRUE;
 				this->Bricks.push_back(obj);
 			}
-			else if (tileData[y][x] > 1)	// Non-solid; now determine its color based on level data
+			else if (rowX[x] > static_cast<GLint>(1))	// Non-solid; now determine its color based on level data
 			{
 				glm::vec3 color = glm::vec3(1.0f); // original: white
-				if (tileData[y][x] == 2)
+				if (rowX[x] == 2)
 					color = glm::vec3(0.2f, 0.6f, 1.0f);
-				else if (tileData[y][x] == 3)
+				else if (rowX[x] == 3)
 					color = glm::vec3(0.0f, 0.7f, 0.0f);
-				else if (tileData[y][x] == 4)
+				else if (rowX[x] == 4)
 					color = glm::vec3(0.8f, 0.8f, 0.4f);
-				else if (tileData[y][x] == 5)
+				else if (rowX[x] == 5)
 					color = glm::vec3(1.0f, 0.5f, 0.0f);
 
 				glm::vec2 pos(unit_width * x, unit_height * y);
